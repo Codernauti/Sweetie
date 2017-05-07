0ad0f44ed4b966ed25949853aef156dc8689febf
EduBic <bic.studio@yahoo.it>
2017-05-07 18:40:03 +0200

    Added first experimental actions screen. Added Actions view and adapter.

--- app/src/main/java/com/sweetcompany/sweetie/Actions/ActionsFragment.java
+++ app/src/main/java/com/sweetcompany/sweetie/Actions/ActionsFragment.java
@@ -1,21 +1,46 @@
 package com.sweetcompany.sweetie.Actions;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.sweetcompany.sweetie.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ActionsFragment extends Fragment {
+
+    private RecyclerView mActionsList;
+    private ActionsAdapter mActionAdapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mActionAdapter = new ActionsAdapter(20);
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View viewHome = inflater.inflate(R.layout.actions_fragment, container, false);
+        View root = inflater.inflate(R.layout.actions_fragment, container, false);
 
-        return viewHome;
+        mActionsList = (RecyclerView) root.findViewById(R.id.actions_list);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        mActionsList.setLayoutManager(layoutManager);
+
+        mActionsList.setAdapter(mActionAdapter);
+
+
+        return root;
     }
 
 

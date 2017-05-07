1ab815f9ddef01869da70603f3f467e50e944fb0
Gheeroppa <federico.ghir@gmail.com>
2017-05-08 00:34:33 +0200

    add Date e Type degli action item

--- app/src/main/java/com/sweetcompany/sweetie/Actions/ActionsAdapter.java
+++ app/src/main/java/com/sweetcompany/sweetie/Actions/ActionsAdapter.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.sweetcompany.sweetie.R;
@@ -15,14 +16,14 @@ import com.sweetcompany.sweetie.R;
  * Created by Eduard on 07/05/2017.
  */
 
-public class ActionsAdapter extends RecyclerView.Adapter<ActionsAdapter.ActionViewHolder> {
+class ActionsAdapter extends RecyclerView.Adapter<ActionsAdapter.ActionViewHolder> {
 
     private static String TAG = "ActionsAdapter";
     private static int VIEW_HOLDER_COUNT = 0;
 
     private int mNumberItems;
 
-    public ActionsAdapter(int numberOfItems) {
+    ActionsAdapter(int numberOfItems) {
         mNumberItems = numberOfItems;
         //mOnClickListener = listener;
     }
@@ -44,6 +45,7 @@ public class ActionsAdapter extends RecyclerView.Adapter<ActionsAdapter.ActionVi
     @Override
     public void onBindViewHolder(ActionViewHolder holder, int position) {
         Log.d(TAG, ":onBindViewHolder() #" + position);
+        holder.title.setText("Title " + position);
     }
 
     @Override
@@ -52,9 +54,19 @@ public class ActionsAdapter extends RecyclerView.Adapter<ActionsAdapter.ActionVi
     }
 
 
-    public class ActionViewHolder extends RecyclerView.ViewHolder {
-        public ActionViewHolder(View itemView) {
+    class ActionViewHolder extends RecyclerView.ViewHolder {
+        TextView title, description, date;
+        ImageView avatar, type;
+
+        ActionViewHolder(View itemView) {
             super(itemView);
+            title = (TextView) itemView.findViewById(R.id.title_action_list_item);
+            description = (TextView) itemView.findViewById(R.id.subtitle_action_list_item);
+            date = (TextView) itemView.findViewById(R.id.date_action_list_item);
+
+            avatar = (ImageView) itemView.findViewById(R.id.image_action_list_item);
+            type = (ImageView) itemView.findViewById(R.id.type_action_list_item);
         }
     }
+
 }

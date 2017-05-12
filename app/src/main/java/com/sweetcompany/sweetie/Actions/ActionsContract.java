8e7cd48e0ba06634529f55f1ba383dc60fd4e5ce
EduBic <bic.studio@yahoo.it>
2017-05-12 15:43:25 +0200

    added floating action button to ActionFragment

--- app/src/main/java/com/sweetcompany/sweetie/Actions/ActionsContract.java
+++ app/src/main/java/com/sweetcompany/sweetie/Actions/ActionsContract.java
@@ -11,8 +11,6 @@ public interface ActionsContract {
     interface View {
         void setPresenter(ActionsContract.Presenter presenter);
         void updateActionsList(List<ActionVM> actionsVM);
-
-        void showCalendarFragment();
     }
 
     interface Presenter {

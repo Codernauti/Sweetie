87b6d52dc82128f3fa5d0bc83b35f81a10064149
luhahaha <luca.soldera@studenti.unipd.it>
2017-05-15 18:42:44 +0200

    Aggiunto inserimento dati di registrazione

--- app/src/main/java/com/sweetcompany/sweetie/Registration/StepTwo.java
+++ app/src/main/java/com/sweetcompany/sweetie/Registration/StepTwo.java
@@ -1,14 +1,20 @@
 package com.sweetcompany.sweetie.Registration;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
@@ -25,6 +31,8 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.sweetcompany.sweetie.MainActivity;
 import com.sweetcompany.sweetie.R;
 
@@ -37,6 +45,12 @@ public class StepTwo extends Fragment implements View.OnClickListener {
     private static final int RC_SIGN_IN = 9001;
 
     private Button mForwardButton;
+    private EditText mUsernameText;
+    private EditText mPhoneText;
+    private RadioButton mRadio;
+    private FirebaseDatabase database;
+    private DatabaseReference mFirebaseReference;
+
 
     public static StepOne newInstance() {
         StepOne stepOne = new StepOne();
@@ -49,15 +63,35 @@ public class StepTwo extends Fragment implements View.OnClickListener {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.register_step_two, container, false);
+
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         // Assign fields
         mForwardButton = (Button) view.findViewById(R.id.fordward_button);
+        mUsernameText = (EditText) view.findViewById(R.id.username_input);
+        mPhoneText = (EditText) view.findViewById(R.id.phone_input);
+        mRadio = (RadioButton)view.findViewById(R.id.radio_button_male);
         // Set click listeners
         mForwardButton.setOnClickListener(this);
+        mPhoneText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
 
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                //TODO aggiungere controllo se il numero è gia presente nel db
+            }
+        });
+
+
+        //inizialize FirebaseDatabase
+        database = FirebaseDatabase.getInstance();
+        mFirebaseReference = database.getReference().child("users");
     }
 
 
@@ -65,6 +99,7 @@ public class StepTwo extends Fragment implements View.OnClickListener {
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.fordward_button:
+                writeDB(v);
                 ((RegisterActivity) getActivity()).registrationCompleted();
                 break;
             default:
@@ -72,5 +107,20 @@ public class StepTwo extends Fragment implements View.OnClickListener {
         }
     }
 
+    private void writeDB(View v){
+        String userId = getIdToken();
+        String mUsername = mUsernameText.getText().toString();
+        String mPhone = mPhoneText.getText().toString();
+        boolean mGender = mRadio.isChecked();
+        User user = new User(mUsername,mPhone,mGender);
+        mFirebaseReference.child(userId).setValue(user);
+    }
+
+    private String getIdToken(){
+        SharedPreferences settings = this.getActivity().getSharedPreferences("token", 0);
+        Log.d("Recupero uid",settings.getString("id","error"));
+        return settings.getString("id","error");
+
+    }
 
 }

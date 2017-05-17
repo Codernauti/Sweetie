0ed1a651b23e5ebbb6525616482a885931bbdd3d
EduBic <bic.studio@yahoo.it>
2017-05-17 22:48:44 +0200

    Added bookmark button to chat and minor GUI fix

--- app/src/main/java/com/sweetcompany/sweetie/Chat/ChatAdapter.java
+++ app/src/main/java/com/sweetcompany/sweetie/Chat/ChatAdapter.java
@@ -1,10 +1,10 @@
 package com.sweetcompany.sweetie.Chat;
 
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.sweetcompany.sweetie.R;
@@ -16,14 +16,15 @@ import java.util.List;
  * Created by ghiro on 16/05/2017.
  */
 
-public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder> {
+class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder> {
 
     private static String TAG = "ChatAdapter";
 
     private List<TextMessageVM> mMessageList = new ArrayList<>();
 
     ChatAdapter(){
-        //mMessageList.add(new TextMessageVM("barabba", true));
+        mMessageList.add(new TextMessageVM("barabba", true));
+        mMessageList.add(new TextMessageVM("Vogliamo BARABBA! vogliamo barabba!!!! BARABBA!", true));
     }
 
     @Override
@@ -39,7 +40,7 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder
     @Override
     public void onBindViewHolder(ChatViewHolder holder, int position) {
         TextMessageVM textMessageVM = mMessageList.get(position);
-        holder.mText.setText(textMessageVM.getText());
+        holder.mTextMessage.setText(textMessageVM.getText());
     }
 
     @Override
@@ -66,13 +67,22 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder
 
 
 
-    class ChatViewHolder extends RecyclerView.ViewHolder {
-        private TextView mText;
+    class ChatViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private TextView mTextMessage;
+        private ImageButton mBookmarkButton;
 
         public ChatViewHolder(View itemView) {
             super(itemView);
 
-            mText = (TextView) itemView.findViewById(R.id.chat_item_text_view);
+            mTextMessage = (TextView) itemView.findViewById(R.id.chat_item_text_view);
+            mBookmarkButton = (ImageButton) itemView.findViewById(R.id.chat_item_bookmark_button);
+
+            mBookmarkButton.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            mBookmarkButton.setSelected(!mBookmarkButton.isSelected());
         }
     }
 }

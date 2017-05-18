f18d4089d6bd6b8f3ebc1b4f0beb2c85cb9b5d86
EduBic <bic.studio@yahoo.it>
2017-05-18 22:53:32 +0200

    refactoring MessageVM hierarchies

--- app/src/main/java/com/sweetcompany/sweetie/Chat/ChatAdapter.java
+++ app/src/main/java/com/sweetcompany/sweetie/Chat/ChatAdapter.java
@@ -1,7 +1,6 @@
 package com.sweetcompany.sweetie.Chat;
 
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,62 +16,57 @@ import java.util.List;
  * Created by ghiro on 16/05/2017.
  */
 
-class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder> {
+class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.TextMessageViewHolder> {
 
     private static final String TAG = "ChatAdapter";
 
-    private List<TextMessageVM> mMessageList = new ArrayList<>();
+    private List<MessageVM> mMessageList = new ArrayList<>();
 
     ChatAdapter(){
 
         // Populate items for TEST
-        mMessageList.add(new TextMessageVM("Amore hai chiamato?", false));
-        mMessageList.add(new TextMessageVM("Dammi 10 minuti e scendo", false));
-        mMessageList.add(new TextMessageVM("Sono in macchina più avanti al palazzo", true));
-        mMessageList.add(new TextMessageVM("Ok", true));
-        mMessageList.add(new TextMessageVM(":) Scendi qualcosa di cibo?o magari da bere", true));
-        mMessageList.add(new TextMessageVM("Se puoi", true));
-        mMessageList.add(new TextMessageVM("Se vuoi torno", true));
-        mMessageList.add(new TextMessageVM("Che cosa ha fatto di male?", true));
-        mMessageList.add(new TextMessageVM("Buongiorno", false));
-        mMessageList.add(new TextMessageVM("Hey! :)", true));
-        mMessageList.add(new TextMessageVM("Allora...?", false));
-        mMessageList.add(new TextMessageVM("Che c'è?", true));
+        mMessageList.add(new TextMessageVM("Amore hai chiamato?", MessageVM.THE_PARTNER));
+        mMessageList.add(new TextMessageVM("Dammi 10 minuti e scendo", MessageVM.THE_PARTNER));
+        mMessageList.add(new TextMessageVM("Sono in macchina più avanti al palazzo", MessageVM.THE_MAIN_USER));
+        mMessageList.add(new TextMessageVM("Ok", MessageVM.THE_MAIN_USER));
+        mMessageList.add(new TextMessageVM(":) Scendi qualcosa di cibo?o magari da bere", MessageVM.THE_MAIN_USER));
+        mMessageList.add(new TextMessageVM("Se puoi", MessageVM.THE_MAIN_USER));
+        mMessageList.add(new TextMessageVM("Se vuoi torno", MessageVM.THE_MAIN_USER));
+        mMessageList.add(new TextMessageVM("Che cosa ha fatto di male?", MessageVM.THE_MAIN_USER));
+        mMessageList.add(new TextMessageVM("Buongiorno", MessageVM.THE_PARTNER));
+        mMessageList.add(new TextMessageVM("Hey! :)", MessageVM.THE_MAIN_USER));
+        mMessageList.add(new TextMessageVM("Allora...?", MessageVM.THE_PARTNER));
+        mMessageList.add(new TextMessageVM("Che c'è?", MessageVM.THE_MAIN_USER));
     }
 
     @Override
     public int getItemViewType(int position) {
-        return mMessageList.get(position).whoIs();
+        return mMessageList.get(position).getWho();
     }
 
     @Override
-    public ChatViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public TextMessageViewHolder onCreateViewHolder(ViewGroup parent, int who) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
 
         View viewToInflate;
-        ChatAdapter.ChatViewHolder viewHolder;
+        TextMessageViewHolder viewHolder;
 
-        switch (viewType) {
-            case TextMessageVM.THE_USER:
-                viewToInflate = inflater.inflate(R.layout.chat_user_list_item, parent, false);
-                viewHolder = new ChatAdapter.ChatViewHolder(viewToInflate, true);
-                break;
-            case TextMessageVM.THE_PARTNER:
-                viewToInflate = inflater.inflate(R.layout.chat_partner_list_item, parent, false);
-                viewHolder = new ChatAdapter.ChatViewHolder(viewToInflate, false);
-                break;
-            default:
-                viewHolder = new ChatAdapter.ChatViewHolder(null, true);
-                Log.d(TAG, "ChatViewHolder not correctly instantiate!");
+        if (who == TextMessageVM.THE_MAIN_USER) {
+            viewToInflate = inflater.inflate(R.layout.chat_user_list_item, parent, false);
+            viewHolder = new TextMessageViewHolder(viewToInflate, who);
+        }
+        else { // THE_PARTNER
+            viewToInflate = inflater.inflate(R.layout.chat_partner_list_item, parent, false);
+            viewHolder = new TextMessageViewHolder(viewToInflate, who);
         }
 
         return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(ChatViewHolder holder, int position) {
-        TextMessageVM textMessageVM = mMessageList.get(position);
-        holder.mTextMessage.setText(textMessageVM.getText());
+    public void onBindViewHolder(TextMessageViewHolder holder, int position) {
+        MessageVM msgVM = mMessageList.get(position);
+        msgVM.configViewHolder(holder);
     }
 
     @Override
@@ -80,32 +74,32 @@ class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder> {
         return mMessageList.size();
     }
 
-    public void addMessage(TextMessageVM message) {
+    public void addMessage(MessageVM message) {
         mMessageList.add(message);
         notifyItemInserted(mMessageList.size());
     }
 
 
-
-    class ChatViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    class TextMessageViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private TextView mTextMessage;
         private ImageButton mBookmarkButton;
 
-        ChatViewHolder(View itemView, boolean isUser) {
+        TextMessageViewHolder(View itemView, int who) {
             super(itemView);
 
-            // TODO: da sistemare, non posso usare un booleano -> usare ViewHolder diverse
-            if (isUser) {
+            if (who == MessageVM.THE_MAIN_USER) {
                 mTextMessage = (TextView) itemView.findViewById(R.id.chat_item_text_view);
                 mBookmarkButton = (ImageButton) itemView.findViewById(R.id.chat_item_bookmark_button);
             }
-            else {  // isPartner
+            else {  // THE_PARTNER
                 mTextMessage = (TextView) itemView.findViewById(R.id.chat_partner_item_text_view);
                 mBookmarkButton = (ImageButton) itemView.findViewById(R.id.chat_partner_item_bookmark_button);
             }
 
             mBookmarkButton.setOnClickListener(this);
         }
+
+        public void setText(String text) { mTextMessage.setText(text);}
 
         @Override
         public void onClick(View v) {

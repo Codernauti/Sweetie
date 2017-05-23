e6a8f6cf5a32aa82757d00e4e6f75a3b0f234b5f
Gheeroppa <federico.ghir@gmail.com>
2017-05-24 00:46:44 +0200

    clean code

--- app/src/main/java/com/sweetcompany/sweetie/Utils/Utility.java
+++ app/src/main/java/com/sweetcompany/sweetie/Utils/Utility.java
@@ -2,6 +2,10 @@ package com.sweetcompany.sweetie.Utils;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.icu.text.DateFormat;
+import android.icu.text.SimpleDateFormat;
+
+import java.util.Date;
 
 /**
  * Created by lucas on 22/05/2017.
@@ -17,5 +21,18 @@ public class Utility {
     static public String getStringPreference(Context context,String name){
         SharedPreferences setting = context.getSharedPreferences(name, 0);
         return setting.getString(name,"error");
+    }
+
+    // TODO
+    // Give dateTime
+    // return data formatted for Actions
+    // es: Give (24/05/17 22:00:01)
+    //     if date of today             return "22:00"
+    //     if date of yesterday         return "TOMORROW/IERI"
+    //     if date past                 return "24/05/17"
+    public String getDateFormattedForActions(String data){
+        String mData = data;
+
+        return mData;
     }
 }

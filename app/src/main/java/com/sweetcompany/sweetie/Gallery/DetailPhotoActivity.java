5d96a12f6ac664e4c132400bda0610d81767331d
NBALB23\Federico <allegro.federico@gmail.com>
2017-05-24 14:55:45 +0200

    gallery activities

---
+++ app/src/main/java/com/sweetcompany/sweetie/Gallery/DetailPhotoActivity.java
@@ -0,0 +1,68 @@
+package com.sweetcompany.sweetie.Gallery;
+
+/**
+ * Created by Federico on 22/05/2017.
+ */
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.Palette;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+import com.sweetcompany.sweetie.R;
+
+
+public class DetailPhotoActivity extends AppCompatActivity {
+
+    public static final String EXTRA_SPACE_PHOTO = "SpacePhotoActivity.SPACE_PHOTO";
+
+    private ImageView mImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gallery_detail_layout);
+
+        mImageView = (ImageView) findViewById(R.id.image);
+        Photo spacePhoto = getIntent().getParcelableExtra(EXTRA_SPACE_PHOTO);
+
+        Glide.with(this)
+                .load(spacePhoto.getUrl())
+                .asBitmap()
+                .error(R.drawable.action_photo_icon)
+                .listener(new RequestListener<String, Bitmap>() {
+
+                    @Override
+                    public boolean onException(Exception e, String model, Target<Bitmap> target, boolean isFirstResource) {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Bitmap resource, String model, Target<Bitmap> target, boolean isFromMemoryCache, boolean isFirstResource) {
+
+                        onPalette(Palette.from(resource).generate());
+                        mImageView.setImageBitmap(resource);
+
+                        return false;
+                    }
+
+                    public void onPalette(Palette palette) {
+                        if (null != palette) {
+                            ViewGroup parent = (ViewGroup) mImageView.getParent().getParent();
+                            parent.setBackgroundColor(palette.getDarkVibrantColor(Color.GRAY));
+                        }
+                    }
+                })
+                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                .into(mImageView);
+
+    }
+
+}
---
+++ app/src/main/java/com/sweetcompany/sweetie/Gallery/Photo.java
@@ -0,0 +1,82 @@
+package com.sweetcompany.sweetie.Gallery;
+
+/**
+ * Created by Federico on 22/05/2017.
+ */
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+
+public class Photo implements Parcelable {
+
+    private String mUrl;
+    private String mTitle;
+
+    public Photo(String url, String title) {
+        mUrl = url;
+        mTitle = title;
+    }
+
+    protected Photo(Parcel in) {
+        mUrl = in.readString();
+        mTitle = in.readString();
+    }
+
+    public static final Creator<Photo> CREATOR = new Creator<Photo>() {
+        @Override
+        public Photo createFromParcel(Parcel in) {
+            return new Photo(in);
+        }
+
+        @Override
+        public Photo[] newArray(int size) {
+            return new Photo[size];
+        }
+    };
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    public void setUrl(String url) {
+        mUrl = url;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public static  Photo[] getPhotos() {
+
+        return new Photo[]{
+                new Photo("http://i.imgur.com/zuG2bGQ.jpg", "Galaxy"),
+                new Photo("http://i.imgur.com/ovr0NAF.jpg", "Space Shuttle"),
+                new Photo("http://i.imgur.com/n6RfJX2.jpg", "Galaxy Orion"),
+                new Photo("http://i.imgur.com/qpr5LR2.jpg", "Earth"),
+                new Photo("http://i.imgur.com/pSHXfu5.jpg", "Astronaut"),
+                new Photo("http://i.imgur.com/3wQcZeY.jpg", "Satellite"),
+                new Photo("http://i.imgur.com/zuG2bGQ.jpg", "Galaxy"),
+                new Photo("http://i.imgur.com/ovr0NAF.jpg", "Space Shuttle"),
+                new Photo("http://i.imgur.com/n6RfJX2.jpg", "Galaxy Orion"),
+                new Photo("http://i.imgur.com/qpr5LR2.jpg", "Earth"),
+                new Photo("http://i.imgur.com/pSHXfu5.jpg", "Astronaut"),
+                new Photo("http://i.imgur.com/3wQcZeY.jpg", "Satellite"),
+        };
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(mUrl);
+        parcel.writeString(mTitle);
+    }
+}

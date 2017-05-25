06935674eec4ce6649723d43fd7bd520d91e0fb9
ghiro <barabba123>
2017-05-25 14:56:50 +0200

    refactor GalleryActivity/Adapter

--- app/src/main/java/com/sweetcompany/sweetie/Gallery/GalleryActivity.java
+++ app/src/main/java/com/sweetcompany/sweetie/Gallery/GalleryActivity.java
@@ -13,6 +13,7 @@ import android.widget.ImageView;
 
 
 import com.bumptech.glide.Glide;
+import com.sweetcompany.sweetie.Gallery.ImageGalleryAdapter;
 import com.sweetcompany.sweetie.R;
 
 /**
@@ -21,87 +22,25 @@ import com.sweetcompany.sweetie.R;
 
 public class GalleryActivity extends AppCompatActivity {
 
+    private ImageGalleryAdapter mGalleryAdapter;
+    private Context mContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gallery_grid_view);
+
+        mGalleryAdapter = new ImageGalleryAdapter(this, Photo.getPhotos());
+        mContext = getApplicationContext();
 
         RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, 3);
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rv_images);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(layoutManager);
 
-        GalleryActivity.ImageGalleryAdapter adapter = new GalleryActivity.ImageGalleryAdapter(this, Photo.getPhotos());
-        recyclerView.setAdapter(adapter);
+        recyclerView.setAdapter(mGalleryAdapter);
 
     }
 
-
-    private class ImageGalleryAdapter extends RecyclerView.Adapter<ImageGalleryAdapter.MyViewHolder>  {
-
-        @Override
-        public ImageGalleryAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
-            Context context = parent.getContext();
-            LayoutInflater inflater = LayoutInflater.from(context);
-
-            // Inflate the layout
-            View photoView = inflater.inflate(R.layout.gallery_item_view, parent, false);
-
-            ImageGalleryAdapter.MyViewHolder viewHolder = new ImageGalleryAdapter.MyViewHolder(photoView);
-            return viewHolder;
-        }
-
-
-        @Override
-        public void onBindViewHolder(ImageGalleryAdapter.MyViewHolder holder, int position) {
-
-            Photo singlePhoto = mPhotos[position];
-            ImageView imageView = holder.mPhotoImageView;
-
-            Glide.with(mContext)
-                    .load(singlePhoto.getUrl())
-                    .placeholder(R.drawable.plus_icon)
-                    .into(imageView);
-        }
-
-        @Override
-        public int getItemCount() {
-            return (mPhotos.length);
-        }
-
-        public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-
-            public ImageView mPhotoImageView;
-
-            public MyViewHolder(View itemView) {
-
-                super(itemView);
-                mPhotoImageView = (ImageView) itemView.findViewById(R.id.iv_photo);
-                itemView.setOnClickListener(this);
-            }
-
-            @Override
-            public void onClick(View view) {
-
-                int position = getAdapterPosition();
-                if(position != RecyclerView.NO_POSITION) {
-                    Photo singlePhoto = mPhotos[position];
-
-                    Intent intent = new Intent(mContext, DetailPhotoActivity.class);
-                    intent.putExtra(DetailPhotoActivity.EXTRA_DETAIL_PHOTO, singlePhoto);
-                    startActivity(intent);
-                }
-            }
-        }
-
-        private Photo[] mPhotos;
-        private Context mContext;
-
-        public ImageGalleryAdapter(Context context, Photo[] galleryPhotos) {
-            mContext = context;
-            mPhotos = galleryPhotos;
-        }
-    }
 }
 

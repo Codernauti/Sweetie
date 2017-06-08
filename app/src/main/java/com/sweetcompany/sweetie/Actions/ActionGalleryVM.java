87db60414ffb76abacc265ad5d59947a00064ac5
ghiro <barabba123>
2017-06-08 16:12:13 +0200

    add icons

--- app/src/main/java/com/sweetcompany/sweetie/Actions/ActionGalleryVM.java
+++ app/src/main/java/com/sweetcompany/sweetie/Actions/ActionGalleryVM.java
@@ -34,6 +34,6 @@ class ActionGalleryVM extends ActionVM {
 
     @Override
     public int getIconId() {
-        return R.drawable.ic_photo_camera_black_48dp;
+        return R.drawable.ic_photo_camera;
     }
 }

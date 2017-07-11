3454b0d24d08d794458716e7dfbd03fa6d168a97
EduBic <bic.studio@yahoo.it>
2017-07-11 16:10:54 +0200

    Added updating of user nodes when he couples with another. Extract static final variable from FbPairingController

---
+++ app/src/main/java/com/sweetcompany/sweetie/Firebase/Constraints.java
@@ -0,0 +1,15 @@
+package com.sweetcompany.sweetie.Firebase;
+
+/**
+ * Created by Eduard on 09-Jul-17.
+ */
+
+interface Constraints {
+
+    String USERS_NODE = "users";
+        String COUPLE_INFO_NODE = "couple-info";
+
+    String COUPLES_NODE = "couples";
+
+    String PAIRING_REQUESTS_NODE = "pairing-requests";
+}

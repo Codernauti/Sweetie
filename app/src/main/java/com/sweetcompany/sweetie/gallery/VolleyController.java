b13c8e4ab0698a6381d536dd46336ed68a1404de
Gheeroppa <federico.ghir@gmail.com>
2017-07-22 11:50:01 +0200

    add Volley singleton

---
+++ app/src/main/java/com/sweetcompany/sweetie/gallery/VolleyController.java
@@ -0,0 +1,57 @@
+package com.sweetcompany.sweetie.gallery;
+
+/**
+ * Created by ghiro on 22/07/2017.
+ */
+
+import android.app.Application;
+import android.text.TextUtils;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class VolleyController extends Application {
+
+    public static final String TAG = VolleyController.class
+            .getSimpleName();
+
+    private RequestQueue mRequestQueue;
+
+    private static VolleyController mInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+    }
+
+    public static synchronized VolleyController getInstance() {
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req, String tag) {
+        // set the default tag if tag is empty
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getRequestQueue().add(req);
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        req.setTag(TAG);
+        getRequestQueue().add(req);
+    }
+
+    public void cancelPendingRequests(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
+        }
+    }
+}

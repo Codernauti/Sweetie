4a1a8e625fb38488abb17149857f74cb2c066ebc
EduBic <bic.studio@yahoo.it>
2017-07-25 18:13:26 +0200

    Added utilities for get and set int from sharedPreferences

--- app/src/main/java/com/sweetcompany/sweetie/utils/Utility.java
+++ app/src/main/java/com/sweetcompany/sweetie/utils/Utility.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.util.Log;
 
+import com.sweetcompany.sweetie.R;
+
 /**
  * Created by lucas on 22/05/2017.
  */
@@ -21,12 +23,16 @@ public class Utility {
     public static final String FUTURE_PARTNER_PAIRING_REQUEST = "futurePartner";
     public static final String DEFAULT_VALUE = "error";
 
+    public static final String KB_HEIGHT = "keyboard_height";
+
+    private static final int DEFAULT_INT_VALUE = 0;
+
     //Method for saving a shared preference
     //How to use: pass the context, a key string and the data string; returns true if successfully saved
     //
      static public boolean saveStringPreference(Context context, String key, String data) {
-         Log.d("Save Preference", key + ": " + data);
-         SharedPreferences settings = context.getSharedPreferences(key, 0);
+         Log.d("Save string preference", key + ": " + data);
+         SharedPreferences settings = context.getSharedPreferences(key, Context.MODE_PRIVATE);
          SharedPreferences.Editor editor = settings.edit();
          editor.putString(key,data);
          return editor.commit();
@@ -34,6 +40,7 @@ public class Utility {
 
     //
     static public boolean checkPreferencesSetted(Context context){
+        // TODO: use equal() !!!
         if(getStringPreference(context, USER_UID) != DEFAULT_VALUE){
             if(getStringPreference(context,MAIL) != DEFAULT_VALUE){
                 return true;
@@ -42,12 +49,40 @@ public class Utility {
         return false;
     }
 
-    //Method for getting a string from shared preferences
-    //How to use: pass the context and one of key strings above
-    //if the result string doesn't exist it returns the string "error"
+    /**
+     * Method for getting a string from shared preferences
+     * How to use: pass the context and one of key strings above
+     * if the result string doesn't exist it returns the string "error"
+     * @param context
+     * @param key
+     * @return
+     */
     static public String getStringPreference(Context context,String key){
-        SharedPreferences setting = context.getSharedPreferences(key, 0);
+        SharedPreferences setting = context.getSharedPreferences(key, Context.MODE_PRIVATE);
         return setting.getString(key, DEFAULT_VALUE);
+    }
+
+    static public boolean saveIntPreference(Context context, String key, int data) {
+        Log.d("Save int preference", key + ": " + data);
+        SharedPreferences settings = context.getSharedPreferences(key, Context.MODE_PRIVATE);
+        return settings.edit()
+                        .putInt(key, data)
+                        .commit();
+    }
+
+    static public int getIntPreference(Context context, String key) {
+        SharedPreferences setting = context.getSharedPreferences(key, Context.MODE_PRIVATE);
+
+        if (key.equals(KB_HEIGHT)) {
+            int data = setting.getInt(key, R.dimen.keyboard_height_default);
+            Log.d("Get int preference", key + ": " + data);
+            return data;
+        }
+        else {
+            int data = setting.getInt(key, DEFAULT_INT_VALUE);
+            Log.d("Get int preference", key + ": " + data);
+            return data;
+        }
     }
 
 }

c600e5e66467c0d325ed932aa7a5ce2a152d0ebf
Gheeroppa <federico.ghir@gmail.com>
2017-08-09 16:34:21 +0200

    add locationUpdater

--- app/src/main/java/com/sweetcompany/sweetie/geogift/GeogiftFragment.java
+++ app/src/main/java/com/sweetcompany/sweetie/geogift/GeogiftFragment.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
@@ -17,11 +19,14 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.GeofencingClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlacePicker;
@@ -38,24 +43,36 @@ import static android.app.Activity.RESULT_OK;
  * Created by ghiro on 07/08/2017.
  */
 
-public class GeogiftFragment  extends Fragment implements GeogiftContract.View {
+public class GeogiftFragment  extends Fragment implements GeogiftContract.View, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     private static final String TAG = "GeogiftFragment";
 
     private static final int PLACE_PICKER_REQUEST = 1;
-    private static int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION =1;
+    private int REQ_PERMISSION_UPDATE = 202;
+
+    //Get the desired interval of this request, in milliseconds.
+    private final int UPDATE_INTERVAL =  8 * 1000; // 30 secs
+    //The system will never provide location updates faster than the minimum of getFastestInterval() and getInterval()
+    private final int FASTEST_INTERVAL = 5 * 1000;  // 5 secs
 
     private Toolbar mToolBar;
     private TextView coordText;
     private Button pickPositionButton;
+    private TextView googleApiConnectionText;
+    private TextView locationConnectionText;
+    private TextView currentText;
 
     private LatLngBounds latLngBounds = null;
     private LatLng latLng = null;
     private boolean mLocationPermissionGranted = false;
 
+    private Location lastLocation;
+    private LocationRequest locationRequest;
+
     private FusedLocationProviderClient mFusedLocationClient;
 
     private GeofencingClient mGeofencingClient;
+    private GoogleApiClient googleApiClient;
 
     private GeogiftContract.Presenter mPresenter;
 
@@ -72,7 +89,12 @@ public class GeogiftFragment  extends Fragment implements GeogiftContract.View {
         super.onCreate(savedInstanceState);
 
         mGeofencingClient = LocationServices.getGeofencingClient(getActivity());
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
+        //mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
+
+        //create GoogleApiClient
+        createGoogleApi();
+        // Call GoogleApiClient connection when starting the Activity
+        googleApiClient.connect();
     }
 
     @Override
@@ -96,56 +118,47 @@ public class GeogiftFragment  extends Fragment implements GeogiftContract.View {
 
         coordText = (TextView) root.findViewById(R.id.coord_text_view);
         pickPositionButton = (Button) root.findViewById(R.id.pick_location_button);
+        googleApiConnectionText = (TextView) root.findViewById(R.id.connection_api_text_view);
+        locationConnectionText = (TextView) root.findViewById(R.id.location_api_text_view);
+        currentText = (TextView) root.findViewById(R.id.current_api_text_view);
 
         pickPositionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                pickPosition();
+                if(checkPermission()) {
+                    pickPosition();
+                }
+                else askPermission();
             }
         });
 
         return root;
     }
 
-    public void pickPosition(){
-        if (ContextCompat.checkSelfPermission(getContext(),
-                android.Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-            mLocationPermissionGranted = true;
-        } else {
-            ActivityCompat.requestPermissions(getActivity(),
-                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
-                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
-        }
-
-        if(mLocationPermissionGranted) {
-            //latLngBounds = new LatLng(mLatitude, mLongitude), new LatLng(mLatitude, mLongitude));
-            try {
-                PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
-                //builder.setLatLngBounds(latLngBounds);
-                Intent i = builder.build(getActivity());
-                startActivityForResult(i, PLACE_PICKER_REQUEST);
-            } catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException e) {
-                Log.e(TAG, String.format("GooglePlayServices Not Available [%s]", e.getMessage()));
-            } catch (Exception e) {
-                Log.e(TAG, String.format("PlacePicker Exception: %s", e.getMessage()));
-            }
-        }
-        else{
-            Toast.makeText(getContext(), getString(R.string.need_location_permission_message), Toast.LENGTH_LONG).show();
+    // Create GoogleApiClient instance
+    private void createGoogleApi() {
+        Log.d(TAG, "createGoogleApi()");
+        if ( googleApiClient == null ) {
+            googleApiClient = new GoogleApiClient.Builder(getContext())
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
         }
     }
 
-    public LatLngBounds getLastLocation(){
-        /*LatLngBounds bounds = null;
-
-            double mLatitude = mLastLocation.getLatitude();
-            double mLongitude = mLastLocation.getLongitude();
-            bounds = new LatLngBounds(
-                    new LatLng(mLatitude, mLongitude), new LatLng(mLatitude, mLongitude));
-
-        return bounds;*/
-        return null;
+    public void pickPosition(){
+        latLngBounds = new LatLngBounds(new LatLng(44.882494, 11.601847), new LatLng(44.909004, 11.613520));
+        try {
+            PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+            builder.setLatLngBounds(latLngBounds);
+            Intent i = builder.build(getActivity());
+            startActivityForResult(i, PLACE_PICKER_REQUEST);
+        } catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException e) {
+            Log.e(TAG, String.format("GooglePlayServices Not Available [%s]", e.getMessage()));
+        } catch (Exception e) {
+            Log.e(TAG, String.format("PlacePicker Exception: %s", e.getMessage()));
+        }
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -175,16 +188,142 @@ public class GeogiftFragment  extends Fragment implements GeogiftContract.View {
     @Override
     public void onResume() {
         super.onResume();
+        // Call GoogleApiClient connection when starting the Activity
+        googleApiClient.connect();
     }
 
     @Override
     public void onPause() {
         super.onPause();
+        // Disconnect GoogleApiClient when stopping Activity
+        googleApiClient.disconnect();
     }
 
     @Override
     public void setPresenter(GeogiftContract.Presenter presenter) {
         mPresenter = presenter;
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.i(TAG, "onConnected()");
+        googleApiConnectionText.setText("Connected");
+        getLastKnownLocation();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.w(TAG, "onConnectionSuspended()");
+        googleApiConnectionText.setText("Suspended");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.w(TAG, "onConnectionFailed()");
+        googleApiConnectionText.setText("Connection failed");
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d(TAG, "onLocationChanged ["+location+"]");
+        currentText.setText("onLocationChanged ["+location+"]");
+        lastLocation = location;
+        writeActualLocation(location);
+    }
+
+    // Get last known location
+    private void getLastKnownLocation() {
+        Log.d(TAG, "getLastKnownLocation()");
+        locationConnectionText.setText("getLastKnownLocation()");
+
+        if ( checkPermission() ) {
+            lastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+            if ( lastLocation != null ) {
+                Log.i(TAG, "LasKnown location. " +
+                        "Long: " + lastLocation.getLongitude() +
+                        " | Lat: " + lastLocation.getLatitude());
+                locationConnectionText.setText("LasKnown location. " +
+                        "Long: " + lastLocation.getLongitude() +
+                        " | Lat: " + lastLocation.getLatitude());
+
+                writeLastLocation();
+                startLocationUpdates();
+            } else {
+                Log.w(TAG, "No location retrieved yet");
+                locationConnectionText.setText("No location retrieved yet");
+                startLocationUpdates();
+            }
+        }
+        else askPermission();
+    }
+
+    // Start location Updates
+    private void startLocationUpdates(){
+        Log.i(TAG, "startLocationUpdates()");
+        locationConnectionText.setText("startLocationUpdates()");
+        locationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY) //Set the priority of the request
+                .setInterval(UPDATE_INTERVAL) //Set the desired interval for active location updates
+                .setFastestInterval(FASTEST_INTERVAL); //Explicitly set the fastest interval for location updates
+
+        if (checkPermission())
+            LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+    }
+
+    private void writeLastLocation() {
+        writeActualLocation(lastLocation);
+    }
+
+    // Write location coordinates on UI
+    private void writeActualLocation(Location location) {
+        //textLat.setText( "Lat: " + location.getLatitude() );
+        //textLong.setText( "Long: " + location.getLongitude() );
+        currentText.setText("Lat: " + location.getLatitude());
+    }
+
+    // Check for permission to access Location
+    private boolean checkPermission() {
+        Log.d(TAG, "checkPermission()");
+        // Ask for permission if it wasn't granted yet
+        return (ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED );
+    }
+
+    // Asks for permission
+    private void askPermission() {
+        Log.d(TAG, "askPermission()");
+        ActivityCompat.requestPermissions(
+                getActivity(),
+                new String[] { android.Manifest.permission.ACCESS_FINE_LOCATION },
+                REQ_PERMISSION_UPDATE
+        );
+    }
+
+    // Verify user's response of the permission requested
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d(TAG, "onRequestPermissionsResult()");
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch ( requestCode ) {
+            case 202: //REQ_PERMISSION
+                {
+                if ( grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED ){
+                    // Permission granted
+                    getLastKnownLocation();
+
+                } else {
+                    // Permission denied
+                    permissionsDenied();
+                }
+                break;
+            }
+        }
+    }
+
+    // App cannot work without the permissions
+    private void permissionsDenied() {
+        Log.w(TAG, "permissionsDenied()");
     }
 
 }

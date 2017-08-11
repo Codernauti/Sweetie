6b7543abe21c8f48cdfcec3d90cada54ada7df42
Gheeroppa <federico.ghir@gmail.com>
2017-08-11 17:39:43 +0200

    clean code geogift fragment layout

--- app/src/main/java/com/sweetcompany/sweetie/geogift/GeogiftFragment.java
+++ app/src/main/java/com/sweetcompany/sweetie/geogift/GeogiftFragment.java
@@ -4,6 +4,7 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -14,12 +15,15 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -64,6 +68,23 @@ public class GeogiftFragment  extends Fragment implements GeogiftContract.View {
 
     private static final String TAG = "GeogiftFragment";
 
+    private Toolbar mToolBar;
+    //location picker topbar
+    private ImageView locationPickerIcon;
+    private TextView locationPickerText;
+    //item selector bar
+    private ImageView messageIconButton;
+    private ImageView photoIconButton;
+    private ImageView heartIconButton;
+    private ImageView messageSelector;
+    private ImageView photoSelector;
+    private ImageView heartSelector;
+    //image selector container
+    private ImageView imageThumb;
+    private ImageView clearImageButton;
+
+    private FloatingActionButton mFabAddGeogift;
+
     public static GeogiftFragment newInstance(Bundle bundle) {
         GeogiftFragment newGeogiftFragment = new GeogiftFragment();
         newGeogiftFragment.setArguments(bundle);
@@ -83,6 +104,46 @@ public class GeogiftFragment  extends Fragment implements GeogiftContract.View {
                              Bundle savedInstanceState) {
         // root is a RelativeLayout
         final ViewGroup root = (ViewGroup) inflater.inflate(R.layout.geogift_fragment, container, false);
+
+        // TODO: is useless to set titleGeogift, Firebase update it also if it is offline
+        String titleGeogift = getArguments().getString(GeogiftActivityTest.GEOGIFT_TITLE);
+        String geogiftUid = getArguments().getString(GeogiftActivityTest.GEOGIFT_DATABASE_KEY);
+        Log.d(TAG, "from Intent GEOGIFT_TITLE: " + titleGeogift);
+        Log.d(TAG, "from Intent GEOGIFT_DATABASE_KEY: " + geogiftUid);
+
+        // initialize toolbar
+        mToolBar = (Toolbar) root.findViewById(R.id.geogift_toolbar);
+        AppCompatActivity parentActivity = (AppCompatActivity) getActivity();
+        parentActivity.setSupportActionBar(mToolBar);
+        parentActivity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        parentActivity.getSupportActionBar().setTitle(titleGeogift);
+
+        messageIconButton = (ImageView) root.findViewById(R.id.item_message_geogift_button);
+        messageIconButton.setClickable(true);
+        photoIconButton = (ImageView) root.findViewById(R.id.item_photo_geogift_button);
+        photoIconButton.setClickable(true);
+        heartIconButton = (ImageView) root.findViewById(R.id.item_heart_geogift_button);
+        heartIconButton.setClickable(true);
+        messageSelector = (ImageView) root.findViewById(R.id.message_geogift_selector);
+        messageSelector.setVisibility(View.GONE);
+        photoSelector = (ImageView) root.findViewById(R.id.photo_geogift_selector);
+        photoSelector.setVisibility(View.GONE);
+        heartSelector = (ImageView) root.findViewById(R.id.heart_geogift_selector);
+        heartSelector.setVisibility(View.GONE);
+
+        /*imageThumb = (ImageView) root.findViewById(R.id.image_thumb_geogift);
+        clearImageButton = (ImageView) root.findViewById(R.id.clear_image_geogift_button);
+
+        mFabAddGeogift = (FloatingActionButton) root.findViewById(R.id.fab_new_geogift);
+        mFabAddGeogift.setClickable(false);*/
+
+        /*// Add listener
+        mFabAddGeogift.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });*/
 
         return root;
     }

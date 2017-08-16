9c687e0a4948598e6998b5d7f8944089a75d0f1f
luhahaha <luca.soldera@studenti.unipd.it>
2017-08-16 18:31:23 +0200

    Aggiunta eliminazione check entry

--- app/src/main/java/com/sweetcompany/sweetie/todolist/CheckEntryViewHolder.java
+++ app/src/main/java/com/sweetcompany/sweetie/todolist/CheckEntryViewHolder.java
@@ -12,7 +12,7 @@ import com.sweetcompany.sweetie.R;
  * Created by lucas on 12/08/2017.
  */
 
-public class CheckEntryViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+public class CheckEntryViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener{
     CheckBox checkBox;
     TextView textView;
 
@@ -23,17 +23,25 @@ public class CheckEntryViewHolder extends RecyclerView.ViewHolder implements Vie
         checkBox = (CheckBox) itemView.findViewById(R.id.checkEntry);
         textView = (TextView) itemView.findViewById(R.id.todolist_textView);
         checkBox.setOnClickListener(this);
+        textView.setOnLongClickListener(this);
     }
 
     void setViewHolderClickListener(OnViewHolderClickListener listener) {
         mListener = listener;
     }
 
+
+
     interface OnViewHolderClickListener {
         void onCheckBoxClicked(int adapterPosition, boolean isBookmarked);
+        void onCheckBoxLongClicked(int adapterPosition);
     }
 
-
+    @Override
+    public boolean onLongClick(View v) {
+        mListener.onCheckBoxLongClicked(getAdapterPosition());
+        return false;
+    }
 
     @Override
     public void onClick(View v) {

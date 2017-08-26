14d7fb6359699a2e260e63af3718564a84ea873f
EduBic <bic.studio@yahoo.it>
2017-08-26 13:43:15 +0200

    Added chat background drawable

---
+++ app/src/main/java/com/sweetcompany/sweetie/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.sweetcompany.sweetie;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Eduard on 26-Aug-17.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}

ca3d7fc272465312e826c6d7325b03966d948d46
Gheeroppa <federico.ghir@gmail.com>
2017-08-30 22:21:29 +0200

    clean code

--- app/src/main/java/com/sweetcompany/sweetie/settings/SettingsFragment.java
+++ app/src/main/java/com/sweetcompany/sweetie/settings/SettingsFragment.java
@@ -1,14 +1,11 @@
 package com.sweetcompany.sweetie.settings;
 
-import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.content.PermissionChecker;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;

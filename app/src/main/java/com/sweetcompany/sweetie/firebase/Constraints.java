bf77f70ecae763f0c98c9b8df489861600949f47
EduBic <bic.studio@yahoo.it>
2017-09-09 15:41:07 +0200

    Fixed bug

--- app/src/main/java/com/sweetcompany/sweetie/firebase/Constraints.java
+++ app/src/main/java/com/sweetcompany/sweetie/firebase/Constraints.java
@@ -75,7 +75,7 @@ interface Constraints {
 
     String GALLERY_PHOTOS = "gallery-photos";
     interface GalleryPhotos {
-        String LAST_UPDATE_DATE = "lastUpdateDate";
+        String DATE_TIME = "dateTime";
         String URI_STORAGE = "uriStorage";
         String PROGRESS = "progress";
         String UPLOADING = "uploading";

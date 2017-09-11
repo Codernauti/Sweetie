b4f9758f55146c7208a13e20fd4b692b9a184ab7
Gheeroppa <federico.ghir@gmail.com>
2017-09-11 14:57:04 +0200

    clean code

--- app/src/main/java/com/sweetcompany/sweetie/geogift/GeogiftMakerFragment.java
+++ app/src/main/java/com/sweetcompany/sweetie/geogift/GeogiftMakerFragment.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
